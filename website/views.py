bc5711286250bfad3edaffc71f18947a2497f956
Ishant sigdel <sigdelishant103@gmail.com>
2025-04-16 21:44:59 +0545

    second commit

--- website/views.py
+++ website/views.py
@@ -1,12 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def home(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
-    return render(request, 'home.html',{ })
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged In!")
+            return redirect('home')
+        else:
+            messages.success(request,"There was an error logging in . Try again later" )
+            return redirect('home')
+    else:
+
+        return render(request, 'home.html',{})
 
 
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "You have been successfully logged out")
+    return redirect('home')
+
+def register_user(request):
+    return render(request, 'register.html', {})
+
+
+
+
+
+
